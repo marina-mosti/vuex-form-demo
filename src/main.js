06045a6f6e826d4f46771bfc977a0e34bb3c3397
Marina Mosti <marina@mosti.com.mx>
2019-11-09 12:00:15 -0500

    Autoload base components

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import BaseInput from './components/BaseInput'
+import BaseSelect from './components/BaseSelect'
+
 Vue.config.productionTip = false
+Vue.component('BaseInput', BaseInput)
+Vue.component('BaseSelect', BaseSelect)
 
 new Vue({
   store,
